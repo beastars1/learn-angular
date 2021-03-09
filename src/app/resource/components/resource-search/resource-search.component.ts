3aa5608e2dceda78d12b66acb5aa820393228823
beastars1 <1229435236@qq.com>
2021-03-09 14:13:46 +0800

    alter keyup to sign for search

--- src/app/resource/components/resource-search/resource-search.component.ts
+++ src/app/resource/components/resource-search/resource-search.component.ts
@@ -1,17 +1,26 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-resource-search',
   templateUrl: './resource-search.component.html',
   styleUrls: ['./resource-search.component.css']
 })
-export class ResourceSearchComponent implements OnInit {
+export class ResourceSearchComponent implements AfterViewInit {
+
+  @ViewChild("searchInput") input!: ElementRef;
 
   @Output() onSearch = new EventEmitter<string>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  // view 初始化之后
+  ngAfterViewInit(): void {
+    fromEvent(this.input.nativeElement, "keyup")
+      .subscribe((e: any) => {
+        // console.log(e.target.value);
+        this.onSearch.emit(e.target.value);
+      })
   }
 
 }
